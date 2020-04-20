b217c89680b0d6843da5b0758cbdccea262aa3cd
alaagamal98 <alaagamal.98@gmail.com>
2020-04-20 20:49:04 +0200

    website

--- views/js/main.js
+++ views/js/main.js
@@ -2,21 +2,19 @@
 (function ($) {
     "use strict";
 
-
     /*==================================================================
-    [ Validate after type ]*/
-    $('.validate-input .input100').each(function(){
+    [ Focus Contact2 ]*/
+    $('.input100').each(function(){
         $(this).on('blur', function(){
-            if(validate(this) == false){
-                showValidate(this);
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
             }
             else {
-                $(this).parent().addClass('true-validate');
+                $(this).removeClass('has-val');
             }
         })    
     })
-  
-  
+
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
@@ -38,11 +36,10 @@
     $('.validate-form .input100').each(function(){
         $(this).focus(function(){
            hideValidate(this);
-           $(this).parent().removeClass('true-validate');
         });
     });
 
-     function validate (input) {
+    function validate (input) {
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
@@ -59,21 +56,13 @@
         var thisAlert = $(input).parent();
 
         $(thisAlert).addClass('alert-validate');
-
-        $(thisAlert).append('<span class="btn-hide-validate">&#xf136;</span>')
-        $('.btn-hide-validate').each(function(){
-            $(this).on('click',function(){
-               hideValidate(this);
-            });
-        });
     }
 
     function hideValidate(input) {
         var thisAlert = $(input).parent();
+
         $(thisAlert).removeClass('alert-validate');
-        $(thisAlert).find('.btn-hide-validate').remove();
     }
-    
     
 
 })(jQuery);
